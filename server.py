2425e45ff6dbee7a85b369e4acfb0ba535e51376
Lvaro Jimenez Carrillo <acarrill@gamma09.aulas.gsyc.urjc.es>
2016-10-17 10:54:03 +0200

    ejercicio 5

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
-
+import sys
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
@@ -14,11 +14,15 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 
     def handle(self):
         self.wfile.write(b"Hemos recibido tu peticion")
+        print("Nuevo cliente IP y puerto:", self.client_address)  
         for line in self.rfile:
             print("El cliente nos manda ", line.decode('utf-8'))
 
+#Puerto del server            
+Port = int(sys.argv[1])
+
 if __name__ == "__main__":
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
+    serv = socketserver.UDPServer(('', Port), EchoHandler)
     print("Lanzando servidor UDP de eco...")
     try:
         serv.serve_forever()
