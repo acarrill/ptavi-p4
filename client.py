ecf68e67c3f1406db94af6123da9210191bfa9fb
acarrill <alvarojcjc@hotmail.es>
2016-10-24 14:24:18 +0200

    Ejercicio 6

--- client.py
+++ client.py
@@ -13,11 +13,15 @@ Port = int(sys.argv[2])
 Line = ' '.join(sys.argv[3:])
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((Server, Port))
-    print("Enviando:", Line)
-    my_socket.send(bytes(Line, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
-
-print("Socket terminado.")
+if __name__ == "__main__":
+	with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+		my_socket.connect((Server, Port))
+		print("Enviando:", Line)
+		if sys.argv[3] == 'register':
+			Line = 'REGISTER sip:' + sys.argv[4] + ' SIP/2.0\r\n\r\n'
+		else:
+			sys.exit('Forma de ejecutar cliente: ip puerto register usuario')
+		my_socket.send(bytes(Line, 'utf-8') + b'\r\n')
+		data = my_socket.recv(1024)
+		print('Recibido -- ', data.decode('utf-8'))
+		print("Socket terminado.")
--- server.py
+++ server.py
@@ -7,22 +7,30 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 import socketserver
 import sys
 
-class EchoHandler(socketserver.DatagramRequestHandler):
-    """
-    Echo server class
-    """
-
-    def handle(self):
-        self.wfile.write(b"Hemos recibido tu peticion")
-        print("Nuevo cliente IP y puerto:", self.client_address)  
-        for line in self.rfile:
-            print("El cliente nos manda ", line.decode('utf-8'))
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
+	"""
+	Echo server class
+	"""
+    #Diccionario como atributo de clase
+	Users = {}
+	
+	def handle(self):
+		self.wfile.write(b"SIP/2.0 200 OK")
+		print("Nuevo cliente IP y puerto:", self.client_address)  
+		Line = self.rfile.readline()
+		Line_Str = Line.decode('utf-8')
+		print("El cliente nos manda ", Line_Str)
+		if Line_Str.split(' ')[0] == 'REGISTER':
+			Addres = Line_Str.split(' ')[1]
+			Addres = Addres.split(':')[1]
+			self.Users[Addres] = self.client_address[0]  
+		print(self.Users)
 
 #Puerto del server            
 Port = int(sys.argv[1])
 
 if __name__ == "__main__":
-    serv = socketserver.UDPServer(('', Port), EchoHandler)
+    serv = socketserver.UDPServer(('', Port), SIPRegisterHandler)
     print("Lanzando servidor UDP de eco...")
     try:
         serv.serve_forever()
