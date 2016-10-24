0d85c2db657041c6ae5de7b1272cc933cc9aef42
acarrill <alvarojcjc@hotmail.es>
2016-10-24 16:42:41 +0200

    Ejercicio 7

--- client.py
+++ client.py
@@ -8,19 +8,25 @@ import socket
 import sys
 
 # Dirección IP del servidor y contenido a enviar
-Server = sys.argv[1]
-Port = int(sys.argv[2])
-Line = ' '.join(sys.argv[3:])
+try:
+	Server = sys.argv[1]
+	Port = int(sys.argv[2])
+	Line = ' '.join(sys.argv[3:])
+	Addres = sys.argv[4]
+	Expires = sys.argv[5]
+	if not str.isdigit(Expires): 
+		raise IndexError
+except IndexError:
+	print("Usage: client.py ip puerto register sip_address expires_value")
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 if __name__ == "__main__":
 	with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
 		my_socket.connect((Server, Port))
-		print("Enviando:", Line)
 		if sys.argv[3] == 'register':
-			Line = 'REGISTER sip:' + sys.argv[4] + ' SIP/2.0\r\n\r\n'
-		else:
-			sys.exit('Forma de ejecutar cliente: ip puerto register usuario')
+			Line = ('REGISTER sip:' + Addres + ' SIP/2.0\r\n' +
+			 		'Expires: ' + Expires + '\r\n\r\n')
+		print("Enviando:", Line)
 		my_socket.send(bytes(Line, 'utf-8') + b'\r\n')
 		data = my_socket.recv(1024)
 		print('Recibido -- ', data.decode('utf-8'))
--- server.py
+++ server.py
@@ -17,13 +17,17 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
 	def handle(self):
 		self.wfile.write(b"SIP/2.0 200 OK")
 		print("Nuevo cliente IP y puerto:", self.client_address)  
-		Line = self.rfile.readline()
-		Line_Str = Line.decode('utf-8')
+		Line_Str = self.rfile.read().decode('utf-8')
 		print("El cliente nos manda ", Line_Str)
+		Expires = Line_Str.split(' ')[3].split('\r')[0]
 		if Line_Str.split(' ')[0] == 'REGISTER':
 			Addres = Line_Str.split(' ')[1]
 			Addres = Addres.split(':')[1]
-			self.Users[Addres] = self.client_address[0]  
+			print(Line_Str.split(' ')[3])
+			self.Users[Addres] = {'IP': self.client_address[0], 
+								  'Expires': Expires}
+		if Expires == '0':
+			del self.Users[Addres]
 		print(self.Users)
 
 #Puerto del server            
